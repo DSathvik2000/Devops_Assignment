deecee24d4b93a6a1d367d60cdfdb8b298498a75
VENKATA SATHVIK DUPAGUNTLA <sathvikdupaguntla2000@gmail.com>
2025-02-10 17:34:26 +0530

    Reads config data from file

---
+++ Config_File_Management/Config_Management.py
@@ -0,0 +1,36 @@
+import configparser
+from ctypes import resize
+from doctest import debug
+import json
+import os 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def load_config(config_file):
+    config = configparser.ConfigParser()
+    if not os.path.exists(config_file):
+        return {"error": f"Configuration file '{config_file}' not found."}
+
+    try:
+
+        config.read(config_file)
+        configdata = {section: dict(config.items(section)) for section in config.sections()}
+        return configdata
+    except FileNotFoundError as e:
+        return {"Error" : e}
+    except configparser.Error as e:
+        return {"error": f"Error reading configuration file: {str(e)}"}
+
+@app.route('/config', methods=['GET'])
+def get_config():
+    config_file = r"path\to\read\config.ini"
+    result = load_config(config_file)
+    if result:
+        with open(r"path\to\write\config_output.json", "w") as f:
+            json.dump(result, f, indent=4)
+        return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
